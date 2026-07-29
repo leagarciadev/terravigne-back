9e4114adef37ff693d43ca543066c2c2d83839f3
unknown <leagarciadev@gmail.com>
2026-07-29 17:08:53 +0200

    wip connection

--- api/src/main/java/com/terravigne/api/hello/HelloController.java
+++ api/src/main/java/com/terravigne/api/hello/HelloController.java
@@ -2,8 +2,10 @@ package com.terravigne.api.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
 	@GetMapping("/hello")
