9564b332cd68c2438724a26e2517ef001c919494
Ravenn2203 <leagarciadev@gmail.com>
2026-07-29 22:41:10 +0200

    connect front and back

--- api/src/main/java/com/terravigne/api/hello/HelloController.java
+++ api/src/main/java/com/terravigne/api/hello/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
 	@GetMapping("/hello")
